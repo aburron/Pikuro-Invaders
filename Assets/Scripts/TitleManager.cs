5496958f250c5ae7a0adab633468d6a4508a9ee2
juaniman99 <juaniman.2000@gmail.com>
2017-01-26 16:14:19 +0100

    26/01/2017 (16:11)
    * Ahora cuando se pasa el puntero del ratón sobre las letras
    de"PLAY[...]", estas se ponen en verde.
    
    * Animación en la ventana principal: la tabla de puntuaciones aparece
    progresivamente como en el juego original.
    
    * POR TERMINAR: Generación de Pikuros en la escena de juegos (me
    encargaré yo).
    
    Además, todas las fuentes de texto han sidoreemplazadas por la fuente
    original de Space Invaders

--- Assets/Scripts/TitleManager.cs
+++ Assets/Scripts/TitleManager.cs
@@ -1,13 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 
 public class TitleManager : MonoBehaviour {
 
+    public GameObject PlayInvaderText;
+
 	public void PlayButton (string sceneName)
 	{
         SceneManager.LoadScene(sceneName);
 	}
+    
+    public void GreenColorChangue(bool estate)  //estate: si es true, pondrá el objeto en verde; Si es false, en blanco;
+    {
+        if(estate == true)
+        {
+            PlayInvaderText.GetComponent<Text>().color = Color.green;
+        }else
+        {
+            PlayInvaderText.GetComponent<Text>().color = Color.white;
+        }
+    }
 }
---
+++ Assets/Scripts/pointsShowTitle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pointsShowTitle : MonoBehaviour {
+
+    public float tiempoDeEspera;    //Esto es lo que tardará en aparecer los objetos.
+    public GameObject[] objetos;    //Aquí se especificarán los objetos que van a aparecer poco a poco.
+
+	void Start () {
+        for(int i = 0; i<objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+        StartCoroutine(show());
+    }
+	
+	public IEnumerator show()
+    {
+        for (int i = 0; i < objetos.Length; i++)
+        {
+            yield return new WaitForSeconds(tiempoDeEspera);
+            objetos[i].SetActive(true);
+
+        }        
+    }
+}
